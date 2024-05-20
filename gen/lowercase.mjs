c6e6867e1d41afa1e049aa6938ef6cf9bad9757b
nazarpunk <nazarpunk@gmail.com>
2024-05-20 14:02:23 +0300

    lowercase

---
+++ gen/lowercase.mjs
@@ -0,0 +1,32 @@
+import path from 'path'
+import fs from 'fs'
+
+const base = path.join('..', 'extract')
+const dest = path.join('..', 'lowercase')
+
+const forDir = parent => {
+    for (const item of fs.readdirSync(parent)) {
+        const current = path.join(parent, item)
+        const stat = fs.statSync(current)
+        if (stat.isDirectory()) {
+            forDir(current)
+            continue
+        }
+        const newdest = path.join(dest, path.relative(base, current).toLowerCase())
+        const dirname = path.dirname(newdest)
+        if (!fs.existsSync(dirname)) fs.mkdirSync(dirname, {recursive: true, mode: '0777'})
+
+        fs.copyFileSync(current, newdest)
+    }
+}
+
+forDir(path.join(base, 'Abilities'))
+forDir(path.join(base, 'Buildings'))
+forDir(path.join(base, 'Doodads'))
+forDir(path.join(base, 'Environment'))
+forDir(path.join(base, 'Objects'))
+forDir(path.join(base, 'PathTextures'))
+forDir(path.join(base, 'ReplaceableTextures'))
+forDir(path.join(base, 'SharedModels'))
+forDir(path.join(base, 'Textures'))
+forDir(path.join(base, 'Units'))
