90d4e3351cecae5112ca22da8a14af7311d01761
nazarpunk <nazarpunk@gmail.com>
2024-05-23 15:45:02 +0300

    lowercase

--- gen/lowercase.mjs
+++ gen/lowercase.mjs
@@ -4,7 +4,28 @@ import fs from 'fs'
 const base = path.join('..', 'extract')
 const dest = path.join('..', 'lowercase')
 
+const rmDir = directoryPath => {
+    if (fs.existsSync(directoryPath)) {
+        fs.readdirSync(directoryPath).forEach((file) => {
+            const curPath = path.join(directoryPath, file)
+            if (fs.lstatSync(curPath).isDirectory()) {
+                rmDir(curPath)
+            } else {
+                fs.unlinkSync(curPath)
+            }
+        })
+        fs.rmdirSync(directoryPath)
+    }
+}
+
+rmDir(dest)
+
 const forDir = parent => {
+    if (!fs.existsSync(parent)) {
+        console.error(`Not Exists parent: ${parent}`)
+        return
+    }
+
     for (const item of fs.readdirSync(parent)) {
         const current = path.join(parent, item)
         const stat = fs.statSync(current)
